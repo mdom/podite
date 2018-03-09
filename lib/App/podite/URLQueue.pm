a170d110314054959fc7abe02f9c6adaf3423073
Mario Domgoergen <mdom@taz.de>
2018-03-09 10:09:17 +0100

    Add parallel downloading

---
+++ lib/App/podite/URLQueue.pm
@@ -0,0 +1,57 @@
+# stolen from https://gist.github.com/jberger/5153008
+# all errors probably by me
+package App::podite::URLQueue;
+use Mojo::Base 'Mojo::EventEmitter';
+use Mojo::UserAgent;
+use Mojo::URL;
+use Mojo::IOLoop;
+
+has queue => sub { [] };
+has ua => sub { Mojo::UserAgent->new( max_redirects => 5 ) };
+has worker => 16;
+has delay => sub { Mojo::IOLoop->delay };
+
+sub add {
+    push @{ shift->queue }, @_;
+}
+
+sub start {
+    my ( $self, $cb ) = @_;
+
+    return if !@{ $self->queue };
+    $self->{running} = 0;
+
+    $self->_refresh;
+
+    return $self->delay;
+}
+
+sub _refresh {
+    my $self = shift;
+
+    my $worker = $self->worker;
+    while ( $self->{running} < $worker
+        and my ($tx, $cb ) = splice(@{ $self->queue }, 0, 2) )
+    {
+        $self->{running}++;
+        my $end = $self->delay->begin;
+
+        if ( !ref($tx) || ! $tx->isa('Mojo::Transaction') ) {
+            $tx = $self->ua->build_tx( GET => $tx );
+        }
+
+        $self->ua->start(
+            $tx,
+            sub {
+                my ( $ua, $tx ) = @_;
+		$cb->( $ua, $tx );
+                # refresh worker pool
+                $self->{running}--;
+                $self->_refresh;
+                $end->();
+            }
+        );
+    }
+}
+
+1;
