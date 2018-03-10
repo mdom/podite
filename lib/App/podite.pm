6d30d318f3a9550126094364e13547e12e1ab88c
Mario Domgoergen <mdom@taz.de>
2018-03-10 07:18:00 +0100

    Add empty base class

---
+++ lib/App/podite.pm
@@ -0,0 +1,23 @@
+package App::podite;
+our $VERSION = "0.01";
+1;
+
+__END__
+
+=head1 NAME
+
+podite - command line podcatcher
+
+=head1 LICENSE
+
+Copyright (C) Mario Domgoergen.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Mario Domgoergen E<lt>mdom@taz.deE<gt>
+
+=cut
+
